7506c82f314ddc69eb97ecf48c364633a892469d
arjunmuruges <110531255+arjunmuruges@users.noreply.github.com>
2022-10-10 16:48:28 +0530

    Add files via upload

---
+++ RestfullApi/Api/CustomInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Arjun\RestfullApi\Api; 
+interface CustomInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getData($value);
+}
